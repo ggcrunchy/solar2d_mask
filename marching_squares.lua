12e2484ff1b478353575a8cc6ae1abbf0e9d6ce1
Steven Johnson <steve@xibalbastudios.com>
2014-12-20 22:12:07 -0600

    Marching squares improvements: options, allowing base directory, color flipping, and toroidal wrapping for the edges

--- marching_squares.lua
+++ marching_squares.lua
@@ -49,11 +49,66 @@ local Clear = UL + UR + LL + LR
 -- No flags set: full --
 local Full = 0
 
+--
+local function DefCorrect (index) return index end
+
+--
+local function AuxCorrect (index, comp, delta)
+	if comp then
+		return comp == 0 and index + delta or index
+	else
+		return index - delta, 1
+	end
+end
+
+--
+local function Correction (pitch, total, how)
+	local ncols = pitch - 2
+
+	--
+	if how == "horizontal" then
+		return function(index, col)
+			return AuxCorrect(index, col, ncols)
+		end
+
+	--
+	elseif how then
+		local space = total - 2 * pitch
+
+		--
+		if how == "vertical" then
+			return function(index, _, row)
+				local ci, one = AuxCorrect(index, row, space)
+
+				return ci, false, one
+			end
+
+		--
+		else
+			return function(index, col, row)
+				local cone, rone
+
+				index, cone = AuxCorrect(index, col, ncols)
+				index, rone = AuxCorrect(index, row, space)
+
+				return index, cone, rone
+			end
+		end
+	end
+
+	return DefCorrect
+end
+
 --- DOCME
-function M.NewGrid (get_object, dim, w, h, ncols, nrows, base_dir)
-	local reel = _NewReel_(dim, w / ncols, h / nrows, base_dir)
+function M.NewGrid (get_object, dim, w, h, ncols, nrows, opts)
+	local reel, clear, full = _NewReel_(dim, w / ncols, h / nrows, opts), Clear, Full
 	local cleared, dirty_cells, ndirty, id = {}, {}, 0, 0
 	local pitch, total = ncols + 2, (ncols + 2) * (nrows + 2)
+	local correct = Correction(pitch, total, opts and opts.wrap)
+
+	if opts and opts.flip_color then
+		clear, full = Full, Clear
+	end
 
 	return function(col, row, clear)
 		-- If a cell is dirtied, flip its state, then add each of the four affected display
@@ -77,35 +132,36 @@ function M.NewGrid (get_object, dim, w, h, ncols, nrows, base_dir)
 			-- Visit each dirty cell, building up a state from the cell's corners.
 			for i = 1, ndirty, 3 do
 				local index, col, row = unpack(dirty_cells, i, i + 2)
+				local cindex, ccol, crow = index, col, row
 
 				for j = 1, 4 do
-					if dirty_cells[-index] ~= id then
-						local ul = cleared[index - pitch - 1] and UL or None
-						local ur = cleared[index - pitch] and UR or None
-						local ll = cleared[index - 1] and LL or None
-						local lr = cleared[index] and LR or None
+					if dirty_cells[-cindex] ~= id then
+						local ul = cleared[correct(index - pitch - 1, col - 1, row - 1)] and UL or None
+						local ur = cleared[correct(index - pitch, ccol, row - 1)] and UR or None
+						local ll = cleared[correct(index - 1, col - 1, crow)] and LL or None
+						local lr = cleared[cindex] and LR or None
 						local state = ul + ur + ll + lr
 
 						-- If all corners were cleared, a cell's object becomes invisible. Otherwise, the
 						-- object remains (or becomes) visible.
-						local object = get_object(col, row, ncols, nrows, arg)
+						local object = get_object(ccol, crow, ncols, nrows, arg)
 
 						if object then
-							object.isVisible = state ~= Clear
+							object.isVisible = state ~= clear
 
 							-- If a cell is full, there is nothing to mask.
-							if state == Full then
+							if state == full then then
 								object:setMask(nil)
 
 							-- Otherwise, apply the mask at the given frame. (For empty cells, it is enough
 							-- that the object was made invisible.)
-							elseif state ~= Clear then
+							elseif state ~= clear then then
 								reel("set", object, state)
 							end
 						end
 
 						-- Mark the cell as visited.
-						dirty_cells[-index] = id
+						dirty_cells[-cindex] = id
 					end
 
 					-- Step to another corner.
@@ -116,6 +172,14 @@ function M.NewGrid (get_object, dim, w, h, ncols, nrows, base_dir)
 							index, col = index + dc, col + dc
 						else
 							index, row = index + pitch, row + 1
+						end
+
+						--
+						if correct ~= DefCorrect then
+							cindex, ccol, crow = correct(index, col <= ncols, row <= nrows)
+							ccol, crow = ccol or col, crow or row
+						else
+							cindex, ccol, crow = index, col, row
 						end
 					end
 				end
@@ -128,7 +192,7 @@ function M.NewGrid (get_object, dim, w, h, ncols, nrows, base_dir)
 end
 
 --- DOCME
-function M.NewReel (dim, dw, dh, base_dir)
+function M.NewReel (dim, dw, dh, opts)
 	local reel = mask.NewReel(dim)
 
 	--
@@ -180,7 +244,6 @@ function M.NewReel (dim, dw, dh, base_dir)
 
 				shape = display.newRect(cgroup, 0, 0, w, h)
 				dx, dy = elem[1] * rmove, elem[2] * rmove
-
 			-- TODO: Circle... (or something curved, anyway)
 			else
 				stroke = display.newCircle(cgroup, 0, 0, dim / 2)
@@ -201,6 +264,7 @@ function M.NewReel (dim, dw, dh, base_dir)
 
 			-- Overlay the shape and its strokes in the same positions. The shape's opacity will be
 			-- the opposite of the background's, whereas the strokes will be translucent.
+
 			shape:setFillColor(fg)
 			stroke:setFillColor(.65)
 			shape:translate(dim / 2 + dx, dim / 2 + dy)
@@ -210,12 +274,14 @@ function M.NewReel (dim, dw, dh, base_dir)
 	end
 
 	--
+	local comp_color = (opts and opts.flip_color) and "black" or "white"
+
 	for state, v in pairs(choices) do
-		reel("frame", MakeFrame, state, v[1] == "white")
+		reel("frame", MakeFrame, state, v[1] == comp_color)
 	end
 
 	--
-	reel("end", "__MarchingSquares" .. dim .. "__.png", base_dir)
+	reel("end", "__MarchingSquares" .. dim .. "__.png", opts and opts.base_dir)
 
 	return reel
 end
